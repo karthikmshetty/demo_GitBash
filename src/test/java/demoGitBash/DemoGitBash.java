e55322fdc95fce52643e8f384951aa50fff0e0ce
karthikmshetty <karthikmshetty108@gmail.com>
2023-06-01 12:56:37 +0530

    hj

--- src/test/java/demoGitBash/DemoGitBash.java
+++ src/test/java/demoGitBash/DemoGitBash.java
@@ -5,6 +5,8 @@ public class DemoGitBash {
 	public static void main(String[] args) {
 System.err.println("hello Git Bash");
 		System.err.println("hello Shetty Bhai");
+		
+		System.out.println(" local childGitBash pushed");
 
 		
 	}
