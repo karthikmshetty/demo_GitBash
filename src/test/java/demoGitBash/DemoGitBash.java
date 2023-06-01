fe1260938a0660417eda1e1507a3d5341a76df0c
karthikmshetty <karthikmshetty108@gmail.com>
2023-06-01 14:17:04 +0530

    msgg

--- src/test/java/demoGitBash/DemoGitBash.java
+++ src/test/java/demoGitBash/DemoGitBash.java
@@ -2,13 +2,14 @@ package demoGitBash;
 
 public class DemoGitBash {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 System.err.println("hello Git Bash");
 		System.err.println("hello Shetty Bhai");
 		
 		System.out.println(" local childGitBash pushed");
 
-		
+		System.out.println(" -----local childGitBash pushed 2-----");
+
 	}
 
 }
